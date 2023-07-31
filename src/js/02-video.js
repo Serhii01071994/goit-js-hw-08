1c3ddb933442bf7e5e6d75e3d53412e8ddce58d5
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-07-31 23:47:11 +0300

    видео плеер сделан

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,61 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+//! Вариант 1
+
+// const CURRENT_TIME = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+
+// const player = new Player(iframe, {
+//   loop: true,
+//   fullscreen: true,
+//   quality: '1080p',
+// });
+
+// const getCurrentTime = function (currentTime) {
+//   const seconds = currentTime.seconds;
+//   localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+// };
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0);
+// player
+//   .setColor('#fff')
+//   .then(function (color) {
+//     console.log('The new color value: #fff');
+//   })
+//   .catch(function (error) {
+//     console.log('An error occurred while setting the color');
+//   });
+
+//! Вариант 2
+
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe, {
+  loop: true,
+  fullscreen: true,
+  quality: '1080p',
+});
+
+const getCurrentTime = function (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+const pauseTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+const initialTime = pauseTime ? pauseTime : 0;
+player.setCurrentTime(initialTime);
+
+player
+  .setColor('#fff')
+  .then(function (color) {
+    console.log('The new color value: #fff');
+  })
+  .catch(function (error) {
+    console.log('An error occurred while setting the color');
+  });
