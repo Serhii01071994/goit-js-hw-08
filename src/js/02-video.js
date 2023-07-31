d6fe855d0b8934983e5d251b999b5df5a752b358
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-08-01 00:00:29 +0300

    готово 2 задания

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,34 +3,6 @@ import throttle from 'lodash.throttle';
 
 //! Вариант 1
 
-// const CURRENT_TIME = 'videoplayer-current-time';
-// const iframe = document.querySelector('iframe');
-
-// const player = new Player(iframe, {
-//   loop: true,
-//   fullscreen: true,
-//   quality: '1080p',
-// });
-
-// const getCurrentTime = function (currentTime) {
-//   const seconds = currentTime.seconds;
-//   localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
-// };
-
-// player.on('timeupdate', throttle(getCurrentTime, 1000));
-
-// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0);
-// player
-//   .setColor('#fff')
-//   .then(function (color) {
-//     console.log('The new color value: #fff');
-//   })
-//   .catch(function (error) {
-//     console.log('An error occurred while setting the color');
-//   });
-
-//! Вариант 2
-
 const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 
@@ -47,10 +19,7 @@ const getCurrentTime = function (currentTime) {
 
 player.on('timeupdate', throttle(getCurrentTime, 1000));
 
-const pauseTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
-const initialTime = pauseTime ? pauseTime : 0;
-player.setCurrentTime(initialTime);
-
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)) || 0);
 player
   .setColor('#fff')
   .then(function (color) {
@@ -59,3 +28,34 @@ player
   .catch(function (error) {
     console.log('An error occurred while setting the color');
   });
+
+//! Вариант 2
+
+// const CURRENT_TIME = 'videoplayer-current-time';
+// const iframe = document.querySelector('iframe');
+
+// const player = new Player(iframe, {
+//   loop: true,
+//   fullscreen: true,
+//   quality: '1080p',
+// });
+
+// const getCurrentTime = function (currentTime) {
+//   const seconds = currentTime.seconds;
+//   localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+// };
+
+// player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+// const pauseTime = JSON.parse(localStorage.getItem(CURRENT_TIME));
+// const initialTime = pauseTime ? pauseTime : 0;
+// player.setCurrentTime(initialTime);
+
+// player
+//   .setColor('#fff')
+//   .then(function (color) {
+//     console.log('The new color value: #fff');
+//   })
+//   .catch(function (error) {
+//     console.log('An error occurred while setting the color');
+//   });
