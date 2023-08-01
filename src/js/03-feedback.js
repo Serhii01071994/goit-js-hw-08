865ab35096efbe11571ef96073db2c458ec07fd3
Serhii01071994 <126762718+Serhii01071994@users.noreply.github.com>
2023-08-01 16:57:26 +0300

    3 задание готово

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input[name="email"]');
+const text = document.querySelector('textarea[name="message"]');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+form.addEventListener('input', throttle(savedForm, 500));
+textArea();
+// сохранение в LocalStorage
+function savedForm(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+// отправка формы
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (email.value === '' || text.value === '') {
+    alert('Все поля должні быть заполнены!');
+  } else {
+    console.log(formData);
+    form.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+  }
+}
+
+// функция на загрузку страницы
+function textArea() {
+  const savedText = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (savedText) {
+    const formDataInput = JSON.parse(savedText);
+    email.value = formDataInput.email ?? '';
+    text.value = formDataInput.message ?? '';
+    formData.email = email.value;
+    formData.message = text.value;
+  }
+}
